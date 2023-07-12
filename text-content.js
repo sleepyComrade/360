618d03207a4ea4dca6970d88660425cf3b985f67
sleepyComrade <ivanovichivanivanov@protonmail.ch>
2023-07-12 22:05:39 +0200

    add text content file

---
+++ text-content.js
@@ -0,0 +1,312 @@
+const textContent = {
+  header: {
+    home: {
+      en: "Home",
+      ru: "",
+      sr: "",
+    },
+    about: {
+      en: "About",
+      ru: "",
+      sr: "",
+    },
+    solutions: {
+      en: "Solutions",
+      ru: "",
+      sr: "",
+    },
+    contact: {
+      en: "Contact",
+      ru: "",
+      sr: "",
+    },
+  },
+  cover: {
+    description: {
+      en: "360 Immersive Tours for your website and Google Maps",
+      ru: "",
+      sr: "360° Imersive Ture za vašu veb stranicu i Google Maps",
+    },
+    button: {
+      en: "Order 360 tour",
+      ru: "",
+      sr: "",
+    },
+  },
+  aboutUs: {
+    title: {
+      en: "ABOUT US",
+      ru: "",
+      sr: "O NAMA",
+    },
+    description: {
+      en: "We are PanoRound, a company based in Montenegro specializing in capturing 360 tours of commercial spaces, cafes, restaurants, spa complexes, apartments, and more, for showcasing on clients' websites and Google Maps. With PanoRound, you can transport your customers virtually and provide them with a captivating preview that drives foot traffic through your doors.",
+      ru: "",
+      sr: "Mi smo PanoRound, kompanija sa sjedištem u Crnoj Gori specijalizovana za snimanje 360 obilazaka poslovnih prostora, kafića, restorana, spa kompleksa, apartmana i još mnogo toga, za prikazivanje na web stranicama klijenata i Google Maps. Uz PanoRound, možete virtuelno transportirati svoje kupce i pružiti im zadivljujući pregled koji tjera pješački promet kroz vaša vrata.",
+    },
+    button: {
+      en: "Contact PanoRound",
+      ru: "",
+      sr: "",
+    },
+  },
+  slider: {
+    experience: {
+      title: {
+        en: "Stunning Immersive Experience",
+        ru: "",
+        sr: "NEVJEROVATNO UPOZNAVANJE SA OKRUŽENJEM",
+      },
+      description: {
+        en: "Our cutting-edge technology captures every detail, creating a visually stunning virtual tour that showcases your space in exquisite detail.",
+        ru: "",
+        sr: "Naša napredna tehnologija zabeleži svaki detalj, stvarajući vizuelno impresivnu virtualnu turu koja prikazuje vaš prostor sa izuzetnom pažnjom prema detaljima.",
+      },
+    },
+    mobile: {
+      title: {
+        en: "Mobile-Friendly Virtual Tours",
+        ru: "",
+        sr: "VIRTUALNE TURE",
+      },
+      description: {
+        en: "Our 360 tours are optimized for mobile viewing, ensuring potential customers can explore your space effortlessly on any device.",
+        ru: "",
+        sr: "Naši 360 obilasci su optimizovani za mobilno pregledanje, omogućavajući potencijalnim kupcima da istraže vaš prostor bez ikakvih napora na bilo kom uređaju.",
+      },
+    },
+    marketing: {
+      title: {
+        en: "Marketing Opportunities",
+        ru: "",
+        sr: "MARKETING POTENCIJALI",
+      },
+      description: {
+        en: "Our tours provide valuable marketing assets that can be shared on your website, social media, and presentations, leaving a lasting impression on potential clients.",
+        ru: "",
+        sr: "Naše ture pružaju dragocjena marketinška sredstva koja se mogu dijeliti na vašoj veb stranici, društvenim medijima i prezentacijama, ostavljajući trajan utisak na potencijalne klijente.",
+      },
+    },
+    googleMaps: {
+      title: {
+        en: "Seamless Google Maps Integration",
+        ru: "",
+        sr: "BEŠAVNA INTEGRACIJA GOOGLE MAPA",
+      },
+      description: {
+        en: "We integrate your 360 tour directly into your Google Maps listing, enhancing your online visibility and making it easily accessible to potential customers.",
+        ru: "",
+        sr: "Integrišemo vaš 360° obilazak direktno u vašu listu na Google Mapsu, unapređujući vašu online vidljivost i čineći je lako dostupnom potencijalnim klijentima.",
+      },
+    },
+  },
+  niches: {
+    title: {
+      en: "niches we work with",
+      ru: "",
+      sr: "INDUSTRIJE SA KOJIMA SARADJUJEMO",
+    },
+    yacht: {
+      title: {
+        en: "YACHT TOURS",
+        ru: "",
+        sr: "",
+      },
+      description: {
+        en: "Our team also offers captivating 360 tours of yachts and luxury boats. With our expertise in capturing immersive experiences, we can showcase the luxurious interiors, deck spaces, and breathtaking views that these vessels offer.",
+        ru: "",
+        sr: "",
+      },
+    },
+    residential: {
+      title: {
+        en: "RESIDENTIAL PROPERTIES",
+        ru: "",
+        sr: "STAMBENE NEKRETNINE",
+      },
+      description: {
+        en: "We also cater to the real estate market, providing virtual tours of apartments, condominiums, and houses for sale or rent. These tours offer prospective buyers or tenants an in-depth virtual tour of the property, allowing them to explore each room and envision themselves living in the space.",
+        ru: "",
+        sr: "Takođe se bavimo tržištem nekretnina pružajući virtuelne ture stanova, kondominijuma i kuća koji su na prodaju ili iznajmljivanje. Ove ture nude potencijalnim kupcima ili zakupcima detaljan virtuelni obilazak nekretnine, omogućavajući im da istraže svaku sobu i zamisle sebe kako žive u tom prostoru.",
+      },
+    },
+    hospitality: {
+      title: {
+        en: "HOSPITALITY AND DINING",
+        ru: "",
+        sr: "GOSTOPRIMSTVO I UGOSTITELJSTVO",
+      },
+      description: {
+        en: "Our services extend to the hospitality industry, offering 360 tours of hotels, resorts, bed and breakfasts, and vacation rentals. For restaurants and cafes, PanoRound creates enticing tours that showcase the interior decor, seating arrangements, and dining atmosphere, enticing customers to visit in person.",
+        ru: "",
+        sr: "Naše usluge se proširuju i na industriju gostoprimstva, nudeći 360 ture hotela, odmarališta, pansiona i iznajmljivanja za odmor. Za restorane i kafiće, PanoRound kreira privlačne ture koje prikazuju unutrašnji dekor, raspored sedenja i atmosferu za objedovanje, privlačeći kupce da ih posete lično.",
+      },
+    },
+    spa: {
+      title: {
+        en: "SPA AND WELLNESS",
+        ru: "",
+        sr: "SPA I WELLNESS",
+      },
+      description: {
+        en: "Spa complexes, wellness centers, and fitness facilities can benefit from immersive tours. By capturing the tranquil ambiance, luxurious amenities, and treatment rooms, these tours entice potential clients to indulge in a relaxing and rejuvenating experience.",
+        ru: "",
+        sr: "Spa kompleksi, wellness centri i fitness objekti mogu imati koristi od uranjajućih tura. Snimanjem mirne atmosfere, luksuznih sadržaja i prostorija za tretmane, ove ture privlače potencijalne klijente da uživaju u opuštajućem i osvežavajućem iskustvu.",
+      },
+    },
+    museum: {
+      title: {
+        en: "MUSEUMS AND GALLERIES",
+        ru: "",
+        sr: "MUZEJI I GALERIJE",
+      },
+      description: {
+        en: "PanoRound is dedicated to preserving and promoting art and culture through immersive 360 tours of museums and galleries. We capture the intricate details of exhibitions, artwork, and installations, allowing virtual visitors to explore the space and admire the artwork from every angle.",
+        ru: "",
+        sr: "PanoRound se posvećuje očuvanju i promociji umetnosti i kulture putem uranjajućih 360 tura muzeja i galerija. Snimamo detalje izložbi, umetničkih dela i instalacija, omogućavajući virtuelnim posetiocima da istraže prostor i dive se umetnosti iz svakog ugla.",
+      },
+    },
+    commersials: {
+      title: {
+        en: "COMMERCIAL SPACES",
+        ru: "",
+        sr: "POSLOVNI PROSTORI",
+      },
+      description: {
+        en: "PanoRound specializes in capturing immersive 360 tours of various commercial spaces, including retail stores, offices, showrooms, and event venues. These tours provide a virtual walkthrough experience, allowing potential customers to explore the space and get a feel for its layout and ambiance.",
+        ru: "",
+        sr: "PanoRound se specijalizuje za snimanje uranjanja 360 tura različitih komercijalnih prostora, uključujući prodavnice, kancelarije, izložbene salone i prostorije za događaje. Ove ture pružaju virtuelno iskustvo obilaska, omogućavajući potencijalnim kupcima da istraže prostor i dobiju utisak o rasporedu i atmosferi.",
+      },
+    },
+  },
+  googleInfo: {
+    description: {
+      en: "Google Virtual Tours, also known as Google Business View or Google Street View, are 360-degree virtual tours that allow users to explore the interior of business establishments and outdoor spaces. These tours provide an immersive experience, simulating the feeling of being present in the location.",
+      ru: "",
+      sr: "Google virtuelne ture, poznate i kao Google Business View ili Google Street View, su ture od 360 stepeni koje omogućavaju korisnicima da istraže unutrašnjost poslovnih zgrada i spoljašnjih prostora. Ove ture pružaju impresivno iskustvo, simulirajući osećaj kao da se fizički nalazite na toj lokaciji.",
+    }
+  },
+  googleTrust: {
+    title: {
+      en: "ADVANTAGES FOR YOUR BUSINESS FROM HAVING A GOOGLE MAPS 360 TOUR OF YOUR PLACE:",
+      ru: "",
+      sr: ""
+    },
+    visibility: {
+      title: {
+        en: "ENHANCED VISIBILITY",
+        ru: "",
+        sr: "POBOLJŠANA VIDLJIVOST"
+      },
+      description: {
+        en: "By featuring a 360 tour on Google Maps, your business becomes more visible to potential customers who are searching for local establishments. It increases your online presence and helps you stand out from competitors.",
+        ru: "",
+        sr: "Sa 360° obilaskom na Google mapama, vaše poslovanje postaje vidljivije potencijalnim kupcima koji traže lokalne objekte. Povećava se vaše prisustvo na mreži i pomaže vam da se istaknete u odnosu na konkurenciju."
+      }
+    },
+    engagement: {
+      title: {
+        en: "INCREASED ENGAGEMENT",
+        ru: "",
+        sr: "POVEĆAN ANGAŽMAN"
+      },
+      description: {
+        en: "A 360 tour provides a highly engaging and interactive experience for users. It allows them to virtually explore your space, navigate through different areas, and get a feel for the atmosphere and amenities. This level of engagement can lead to increased interest and higher conversion rates.",
+        ru: "",
+        sr: "360° obilazak korisnicima pruža izuzetno zanimljivo i interaktivno iskustvo. Omogućava im da virtuelno istraže vaš prostor, kreću se kroz različita područja i dožive atmosferu i pogodnosti. Ovaj nivo angažmana može dovesti do većeg interesovanja i povećanja stope konverzije."
+      }
+    },
+    credibility: {
+      title: {
+        en: "BUILD TRUST AND CREDIBILITY",
+        ru: "",
+        sr: "GRADNJA POVERENJA I KREDIBILITETA"
+      },
+      description: {
+        en: "Offering a 360 tour on Google Maps adds a layer of transparency and authenticity to your business. It allows potential customers to see your place in its entirety, creating trust and confidence in what you have to offer. It helps to establish credibility and encourages people to choose your business over others.",
+        ru: "",
+        sr: "Pružanje 360° obilaska na Google mapama dodaje sloj transparentnosti i autentičnosti vašem poslovanju. Potencijalni kupci mogu videti vaše prostorije u celosti, što gradi poverenje i veru u ono što nudite. Pomaže u uspostavljanju kredibiliteta i podstiče ljude da izaberu vašu firmu u odnosu na druge."
+      }
+    },
+    convenience: {
+      title: {
+        en: "CONVENIENCE FOR POTENTIAL CUSTOMERS",
+        ru: "",
+        sr: "POGODNOST ZA POTENCIJALNE KUPCE"
+      },
+      description: {
+        en: "With a Google Maps 360 tour, potential customers can virtually visit your place anytime, anywhere. It saves them time and effort by providing a realistic preview before they make a decision to visit in person. This convenience can attract more visitors and increase foot traffic to your establishment.",
+        ru: "",
+        sr: "Sa 360° obilaskom na Google mapama, potencijalni kupci mogu virtuelno posetiti vaše prostorije bilo kada i bilo gde. To štedi njihovo vreme i trud, pružajući im realističan uvid pre nego što donesu odluku o poseti uživo. Ova pogodnost može privući veći broj posetilaca i povećati protok ljudi do vašeg objekta."
+      }
+    },
+    rankings: {
+      title: {
+        en: "BETTER SEARCH ENGINE RANKINGS",
+        ru: "",
+        sr: "BOLJE RANGIRANJE NA PRETRAŽIVAČIMA"
+      },
+      description: {
+        en: "Google values high-quality and engaging content. By offering a 360 tour on Google Maps, you enhance the content value of your listing, potentially improving your search engine rankings. This can lead to increased organic visibility and more exposure to a wider audience.",
+        ru: "",
+        sr: "Google ceni visokokvalitetan i zanimljiv sadržaj. Prisustvo 360° obilaska na Google mapama povećava vrednost vašeg unosa i potencijalno poboljšava rangiranje na pretraživačima. To može rezultirati povećanom organskom vidljivošću i većom izloženošću široj publici."
+      }
+    }
+  },
+  contact: {
+    form: {
+      title: {
+        en: "Contact Form",
+        ru: "",
+        sr: "Kontakt PanoRound"
+      },
+      description: {
+        en: "Please share your details and requests using the form below and we will get in touch with you as soon as possible.",
+        ru: "",
+        sr: "Molimo Vas da podijelite svoje podatke i zahtjeve koristeći obrazac ispod i mi ćemo se što prije javiti."
+      },
+      firstName: {
+        en: "First Name",
+        ru: "",
+        sr: ""
+      },
+      lastName: {
+        en: "Last Name",
+        ru: "",
+        sr: ""
+      },
+      phone: {
+        en: "Phone",
+        ru: "",
+        sr: ""
+      },
+      email: {
+        en: "E-mail",
+        ru: "",
+        sr: ""
+      },
+      comment: {
+        en: "Comment",
+        ru: "",
+        sr: ""
+      },
+      button: {
+        en: "Send",
+        ru: "",
+        sr: ""
+      }
+    },
+    ourInfo: {
+      title: {
+        en: "Contact Us",
+        ru: "",
+        sr: "Kontaktirajte nas"
+      },
+      description: {
+        en: "We cover the whole of the Montenegro regions.\nFor pricing, orders and suggestions please use our form, phone number or email to contact us. Include as much information about your request, describing location and your goals for the 360 virtual tour.",
+        ru: "",
+        sr: "Pokrivamo sve regione Crne Gore.\nZa cene, narudžbe i sugestije, molimo koristite naš obrazac, telefonski broj ili e-mail kako biste nas kontaktirali. Uključite što više informacija o vašem zahtevu, opisujući lokaciju i ciljeve za 360 virtualnu turu."
+      }
+    }
+  }
+};
